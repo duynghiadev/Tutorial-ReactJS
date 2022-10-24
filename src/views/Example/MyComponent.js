3ef9682cc6f1d3c081159bf5753ebfc70d595eb7
duynghia-123 <duynghia22302@gmail.com>
2022-10-25 04:01:11 +0700

    State - Kiểm Soát Trạng Thái Ứng Dụng React.JS

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,11 +1,32 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "kunhi",
+    channel: "Nghia Basil",
+  };
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h5>Hello MyComponent Duy Nghia</h5>
-      </div>
+      <>
+        <div className="Header first">
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          My name is {this.state.name}
+        </div>
+        <div className="Header second">
+          <h4>Hello MyComponent {this.state.channel}</h4>
+        </div>
+      </>
     );
   }
 }
