eb8cc0c8e588c4b3e8207aa133e6993496419162
duynghia-123 <duynghia22302@gmail.com>
2022-10-26 02:08:43 +0700

    Split Components - Chia cắt Components Trong React.JS

---
+++ src/views/Example/AddComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class AddComponent extends Component {
+  state = {
+    titleJob: "",
+    salary: "",
+  };
+
+  handleChangeTitleJob = (event) => {
+    this.setState({
+      titleJob: event.target.value,
+    });
+  };
+
+  handleChangeSalary = (event) => {
+    this.setState({
+      salary: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(">>> check data input: ", this.state);
+  };
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="fname">Job't title:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.titleJob}
+          onChange={(event) => this.handleChangeTitleJob(event)}
+        />
+        <br />
+        <label htmlFor="lname">Salary:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.salary}
+          onChange={(event) => this.handleChangeSalary(event)}
+        />
+        <br />
+        <br />
+        <input type="submit" onClick={(event) => this.handleSubmit(event)} />
+      </form>
+    );
+  }
+}
+
+export default AddComponent;
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,10 +1,9 @@
 import React from "react";
+import AddComponent from "./AddComponent";
 import ChildComponent from "./ChildComponent";
 
 class MyComponent extends React.Component {
   state = {
-    firstName: "",
-    lastName: "",
     arrJobs: [
       {
         id: "abcJob1",
@@ -24,52 +23,12 @@ class MyComponent extends React.Component {
     ],
   };
 
-  handleChangeFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value,
-    });
-  };
-
-  handleChangeLastName = (event) => {
-    this.setState({
-      lastName: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(">>> check data input: ", this.state);
-  };
-
   render() {
     return (
       <>
-        <form>
-          <label htmlFor="fname">First name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.firstName}
-            onChange={(event) => this.handleChangeFirstName(event)}
-          />
-          <br />
-          <label htmlFor="lname">Last name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.lastName}
-            onChange={(event) => this.handleChangeLastName(event)}
-          />
-          <br />
-          <br />
-          <input type="submit" onClick={(event) => this.handleSubmit(event)} />
-        </form>
-        <ChildComponent
-          name={this.state.firstName}
-          age={"20"}
-          address={"Da Nang"}
-          arrJobs={this.state.arrJobs}
-        />
+        <AddComponent />
+
+        <ChildComponent arrJobs={this.state.arrJobs} />
       </>
     );
   }
