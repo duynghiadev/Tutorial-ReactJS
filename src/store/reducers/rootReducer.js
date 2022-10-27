b35eb75f9e35437d51cd98dc743d081a18ef18e0
duynghia-123 <duynghia22302@gmail.com>
2022-10-28 05:59:44 +0700

    Setup Redux Vào Dự Án React

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  users: [
+    { id: 1, name: "Duy Nghia" },
+    { id: 2, name: "Basil" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
