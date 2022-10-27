40a1e8eba42e4fa2e69f533acd677fee9513ae15
duynghia-123 <duynghia22302@gmail.com>
2022-10-27 09:56:36 +0700

    Importing Image - Sử Dụng Hình Ảnh Trong Ứng Dụng React

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import logo from "../../assets/images/logoChannel.jpg";
 
 class Home extends Component {
   componentDidMount() {
@@ -11,7 +12,14 @@ class Home extends Component {
 
   render() {
     console.log(">>> check props: ", this.props);
-    return <div>Hello Home from HomePage with Duy Nghia</div>;
+    return (
+      <>
+        <div>Hello Home from HomePage with Duy Nghia</div>
+        <div>
+          <img src={logo} alt="avatar" />
+        </div>
+      </>
+    );
   }
 }
 
