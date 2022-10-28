bce61be9135aff0d44f6a22d5be3fc3de3fcbdd7
duynghia-123 <duynghia22302@gmail.com>
2022-10-28 10:57:27 +0700

    ÁP DỤNG REDUX ĐỂ THÊM/XÓA DỮ LIỆU REACT

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -4,10 +4,30 @@ const initState = {
     { id: 2, name: "Basil" },
     { id: 3, name: "Learn React Redux with Duy Nghia" },
   ],
+  posts: [],
 };
 
 const rootReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "DELETE_USER":
+      let users = state.users;
+      users = users.filter((item) => item.id !== action.payload.id);
+      return {
+        ...state,
+        users,
+      };
+
+    case "CREATE_USER":
+      let id = Math.floor(Math.random() * 10000);
+      let user = { id: id, name: `random - ${id}` };
+      return {
+        ...state,
+        users: [...state.users, user],
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -11,8 +11,18 @@ class Home extends Component {
     // }, 3000);
   }
 
+  handleDeleteUser = (user) => {
+    console.log("check user delete: ", user);
+    this.props.deleteUserRedux(user);
+  };
+
+  handleCreateUser = () => {
+    this.props.addUserRedux();
+  };
+
   render() {
     console.log(">>> check props data Redux: ", this.props.dataRedux);
+    let listUsers = this.props.dataRedux;
     return (
       <>
         <div>Hello Home from HomePage with Duy Nghia</div>
@@ -28,6 +38,20 @@ class Home extends Component {
             alt="avatar"
           />
         </div>
+        <div>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name}
+                  &nbsp;
+                  <span onClick={() => this.handleDeleteUser(item)}>x</span>
+                </div>
+              );
+            })}
+          <button onClick={() => this.handleCreateUser()}>Add new</button>
+        </div>
       </>
     );
   }
@@ -39,4 +63,12 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Color(Home));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteUserRedux: (userDelete) =>
+      dispatch({ type: "DELETE_USER", payload: userDelete }),
+    addUserRedux: () => dispatch({ type: "CREATE_USER" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
